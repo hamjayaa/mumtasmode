20e30acb1e247034291df3598a194187e3c61550
hamjayaa <bizhamzah@gmail.com>
2025-09-24 15:25:43 +0700

    Update main.js

--- main.js
+++ main.js
@@ -145,7 +145,7 @@ function createIconsSafe(options = {}) {
 
 // Fungsi untuk memperbarui logo berdasarkan tema
 function updateLogo() {
-  const LIGHT_LOGO_URL = "https://res.cloudinary.com/doqzgozyt/image/upload/v1758543444/logo_web_dceb0e.png";
+  const LIGHT_LOGO_URL = "https://res.cloudinary.com/doqzgozyt/image/upload/v1758699510/logo_light_a77nhs.png";
   const DARK_LOGO_URL = "https://res.com/doqzgozyt/image/upload/v1758518959/emas_ong_ntxmg8.png";
   const logoImg = document.getElementById('mumtas-logo');
   if (!logoImg) return;
